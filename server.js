dad287359d2bea0b03f68c09a12ac1ed296018bc
Mukesh Kumar <mk3304413@gmail.com>
2026-01-11 00:28:20 +0530

    Update server.js with environment variable support

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     console.log('Connected')
 })
