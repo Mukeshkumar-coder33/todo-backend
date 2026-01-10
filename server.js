189817d600ac883c758f1cf046267b78e1113d43
Mukesh Kumar <mk3304413@gmail.com>
2026-01-11 00:20:44 +0530

    Update server.js with environment variable support

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose=require('mongoose'); 
 const cors=require('cors');
+require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-mongoose.connect('mongodb://Localhost:27017/mern-app')
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 .then(()=>{
     console.log('Connected')
 })
